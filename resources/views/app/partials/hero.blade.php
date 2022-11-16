938e9d316f33399507a39f2693b971908a624ffb
UtkinaDariaDev <UtkinaDariaDev@proton.me>
2022-11-16 12:40:52 +0500

    fix ref

---
+++ resources/views/app/partials/hero.blade.php
@@ -0,0 +1,18 @@
+<div class="hero" style="background-image: url(/img/123064.jpg);">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-12">
+                <div class="hero__content text-center">
+                    <div class="hero__content-tag">
+                        <a href="#">Вкусные рецепты! (мое первое laravel-приложение)</a>
+                    </div>
+                    <h2><a href="#">Linux & Programming Cookbook!</a></h2>
+
+                    <div class="hero__content-tag">
+                        <a href="#">just-for-fun-myapps.ru</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
